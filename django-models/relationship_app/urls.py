565cb187b2cadd63565586cf633961b06cfeff9e
solomon <solonjogo@gmail.com>
2024-11-06 16:40:33 +0300

    task2

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
     
     # Updated Login and Logout views with `template_name` specified in `urls.py`
-    path('login/', views.UserLoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', views.UserLogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('login/', views.UserLoginView.as_view(template_name='relationship_app/templates/login.html'), name='login'),
+    path('logout/', views.UserLogoutView.as_view(template_name='relationship_app/templates/logout.html'), name='logout'),
 
     # Registration URL
     path('register/', views.UserRegisterView.as_view(), name='register'),
