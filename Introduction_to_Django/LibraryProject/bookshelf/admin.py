a26a6abbaf77f0fa2dc216505a650ce66932a8be
John <jmnjogo@gmail.com>
2024-11-06 01:14:26 -0800

    task 0

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Book
-
-# Register your models here.
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # Fields to display in the list view
-    search_fields = ('title', 'author')  # Enable search by title and author
-    list_filter = ('publication_year',)  # Enable filtering by publication year
--- Introduction_to_Django/LibraryProject/bookshelf/retrieve.md
+++
@@ -1,7 +0,0 @@
-# Retrieve the Book instance
-
-```python
-# Retrieving the book we just created
-book = Book.objects.get(title="1984")
-print(book.title, book.author, book.publication_year)
-1984 George Orwell 1949
