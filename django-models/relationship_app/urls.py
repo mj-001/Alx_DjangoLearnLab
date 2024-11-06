33124a4d342ca99c9f2ac6276a0f263628e8d58f
solomon <solonjogo@gmail.com>
2024-11-06 16:35:25 +0300

    task2

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -7,13 +7,21 @@ from .views import add_book, edit_book, delete_book
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
     path('admin-view/', admin_view, name='admin_view'),
     path('librarian-view/', librarian_view, name='librarian_view'),
     path('member-view/', member_view, name='member_view'),
     path('book/add_book/', add_book, name='add_book'),
     path('book/<int:pk>/edit_book/', edit_book, name='edit_book'),
     path('book/<int:pk>/delete/', delete_book, name='delete_book'),
+]
+
+# relationship_app/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
 ]
