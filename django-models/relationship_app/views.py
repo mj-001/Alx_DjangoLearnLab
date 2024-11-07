80a7665d53e41aedb42e2bfb1a1254fc78637315
solomon <solonjogogmail.com>
2024-11-07 14:13:18 +0300

    task3

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -39,17 +39,6 @@ class UserRegisterView(CreateView):
     template_name = 'relationship_app/register.html'
     success_url = reverse_lazy('login')
 
-
-# Helper functions for role checks
-def is_admin(user):
-    return user.userprofile.role == 'Admin'
-
-def is_librarian(user):
-    return user.userprofile.role == 'Librarian'
-
-def is_member(user):
-    return user.userprofile.role == 'Member'
-
 # Admin View
 @user_passes_test(is_admin)
 def admin_view(request):
