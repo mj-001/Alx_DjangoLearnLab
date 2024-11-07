e353ddc7097a9dceba82d7d239df14994c537331
solomon <solonjogogmail.com>
2024-11-07 14:28:55 +0300

    task

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -8,7 +8,8 @@ from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.decorators import user_passes_test, login_required
 from django.contrib.auth import login
 from .forms import BookForm
-from .models import Book, Library
+from .models import Book
+from .models import Library
 
 # Helper functions for role checks
 def user_is_admin(user):
