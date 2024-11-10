09945766693a9946a03339bb976c93ea11492dae
solomon <solonjogogmail.com>
2024-11-10 17:42:03 +0300

    maaahn

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -30,7 +30,7 @@ def is_librarian(user):
     return user.userprofile.role == 'LIBRARIAN'
 
 def is_member(user):
-    return user.userprofile.role == 'MEMBER'
+    return user.userprofile.role == 'Member'
 
 @user_passes_test(is_admin)
 def admin_view(request):
