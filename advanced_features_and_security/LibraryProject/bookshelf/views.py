7c4ffe9e2a987d145d3f6db47f6d754529a5a644
solomon <solonjogo@gmail.com>
2024-11-17 22:19:15 +0300

    task1

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponseForbidden
+from .models import Article
 
 # Create your views here.
+
+@permission_required('your_app_name.can_view', raise_exception=True)
+def article_list(request):
+    articles = Article.objects.all()
+    return render(request, 'article_list.html', {'articles': articles})
+
+@permission_required('your_app_name.can_create', raise_exception=True)
+def article_create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        Article.objects.create(title=title, content=content)
+        return redirect('article_list')
+    return render(request, 'article_form.html')
+
+@permission_required('your_app_name.can_edit', raise_exception=True)
+def article_edit(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == 'POST':
+        article.title = request.POST.get('title')
+        article.content = request.POST.get('content')
+        article.save()
+        return redirect('article_list')
+    return render(request, 'article_form.html', {'article': article})
+
+@permission_required('your_app_name.can_delete', raise_exception=True)
+def article_delete(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    article.delete()
+    return redirect('article_list')
