0aa076d212e666ded220d9225e9d2b89402bed40
solomon <solonjogo@gmail.com>
2024-11-24 10:30:06 +0300

    task2

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -3,9 +3,13 @@ from .views import BookList
 from rest_framework.routers import DefaultRouter
 from . import views 
 
-urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),  # Note: Correct 'urlpatterns'
-]
 
 router = DefaultRouter()
 router.register(r'books', views.BookList, basename='book-list')
+
+urlpatterns = [
+    path('books/', views.BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),
+]
+
+
