5d15c16ecbef0a65170be1c5b724f84056591e9f
john <jmnjogo@gmail.com>
2024-12-08 14:44:53 +0300

    task1

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 from registration import views as reg_views
 
 urlpatterns = [
-    path('login/', include('django.contrib.auth.urls')),  # Built-in login/logout views
+    path('login/', include('django.contrib.auth.urls')),  
+    path('logout/', include('django.contrib.auth.urls')), 
     path('register/', reg_views.register, name='register'),
     path('profile/', reg_views.profile_view, name='profile'),
 ]
