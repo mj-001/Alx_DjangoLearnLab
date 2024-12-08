3b37972c6b361778ff704ee714f5a461e3642270
john <jmnjogo@gmail.com>
2024-12-08 15:06:58 +0300

    task1

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path, include
-from registration import views as reg_views
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/', include('django.contrib.auth.urls')),  
-    path('logout/', include('django.contrib.auth.urls')), 
-    path('register/', reg_views.register, name='register'),
-    path('profile/', reg_views.profile_view, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
 ]
