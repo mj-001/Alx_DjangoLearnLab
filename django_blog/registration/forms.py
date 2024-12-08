0f5f3e01ff9ada882cfbac00548f5cd638e519a7
john <jmnjogo@gmail.com>
2024-12-08 15:16:00 +0300

    task1

--- django_blog/registration/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from django.contrib.auth.models import User
-
-class ProfileForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['email']  # You can add more fields like first_name, last_name, etc.
--- django_blog/registration/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import register, profile_view
-
-urlpatterns = [
-    path('register/', register, name='register'),
-    path('profile/', profile_view, name='profile'),
-]
