f27d6a0674f4c6b477aac6d18bae5104d5338142
john <jmnjogo@gmail.com>
2024-12-15 21:40:55 +0300

    task4

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -81,10 +81,15 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test_db',
+        'USER': 'test_user',
+        'PASSWORD': 'test_password',
+        'HOST': 'localhost',  # Example: 'localhost' or 'db.example.com'
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
+
 
 
 # Password validation
