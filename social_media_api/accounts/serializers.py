dd9c4d197412f394aea1779d94a1c207ff2840e3
john <jmnjogo@gmail.com>
2024-12-15 10:29:59 +0300

    task0

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'bio', 'profile_picture', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
